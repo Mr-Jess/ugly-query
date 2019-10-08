8087ccbc89379ce668cd674c34cfa9dccbaabec8
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-08 15:56:39 -0400

    started to make project ugly

---
+++ main.js
@@ -0,0 +1,16 @@
+const newHeader = document.querySelector('h1');
+newHeader.style.color = 'darkred';
+newHeader.style.fontSize = '50px';
+
+
+const newHeader2 = document.querySelector('h3');
+newHeader2.style.color = 'purple';
+newHeader2.style.fontSize = '100px';
+
+const newParagraph = document.querySelector('p');
+newParagraph.style.color = 'yellow';
+newParagraph.style.fontFamily = 'zapfino';
+newParagraph.style.fontSize = '25px';
+
+const wallpaper = document.querySelector('body')
+wallpaper.style.backgroundImage = 'url("https://bloody-disgusting.com/wp-content/uploads/2017/12/predator.jpg")'
