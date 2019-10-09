db289ac6e7f1214e18530a79446c7a1a1ceff66c
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-08 20:52:54 -0400

    created functions

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+//test project
+const wallpaper = document.querySelector('body')
+wallpaper.style.backgroundImage = 'url("https://bloody-disgusting.com/wp-content/uploads/2017/12/predator.jpg")'
+
 const newHeader = document.querySelector('h1');
 newHeader.style.color = 'darkred';
 newHeader.style.fontSize = '50px';
-
 
 const newHeader2 = document.querySelector('h3');
 newHeader2.style.color = 'purple';
@@ -12,5 +15,18 @@ newParagraph.style.color = 'yellow';
 newParagraph.style.fontFamily = 'zapfino';
 newParagraph.style.fontSize = '25px';
 
-const wallpaper = document.querySelector('body')
-wallpaper.style.backgroundImage = 'url("https://bloody-disgusting.com/wp-content/uploads/2017/12/predator.jpg")'
+const margin = document.querySelector('li');
+margin.style.color = 'pink';
+margin.style.fontFamily = 'times new roman'
+margin.style.fontSize = '100px';
+
+const newHeader3 = document.querySelector('ul');
+newHeader3.style.color = 'orange';
+newHeader3.style.fontSize = '500px';
+
+const things = document.querySelector('ol');
+things.style.color = 'limegreen';
+things.style.fontSize = '700px';
+
+const image1 = document.querySelector('')
+image1.style.backgroundImage = 'url("https://media.giphy.com/media/l2YWwrmiGxD9SQeVq/giphy.gif)'
