6a2df2080803e66f640bcda7af71194dcd1110fe
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-09 11:02:46 -0400

    created new gif and fixed minor text

--- main.js
+++ main.js
@@ -6,24 +6,31 @@ const newHeader = document.querySelector('h1');
 newHeader.style.color = 'darkred';
 newHeader.style.fontSize = '50px';
 
-const newHeader2 = document.querySelector('h3');
-newHeader2.style.color = 'purple';
-newHeader2.style.fontSize = '100px';
+const smallHeading2 = document.querySelector('#newHeader2');
+smallHeading2.style.color = 'purple';
+smallHeading2.style.fontSize = '100px';
+
+const smallHeading3 = document.querySelector('#newHeader3');
+smallHeading3.style.color = 'orange';
+smallHeading3.style.fontSize = '300px';
 
 const newParagraph = document.querySelector('p');
 newParagraph.style.color = 'yellow';
 newParagraph.style.fontFamily = 'zapfino';
 newParagraph.style.fontSize = '25px';
-newParagraph.style.backgroundImage = 'url("https://d1ic4altzx8ueg.cloudfront.net/finder-au/wp-uploads/2018/07/multiplafet.jpg)';
+newParagraph.style.backgroundImage = 'url("https://d1ic4altzx8ueg.cloudfront.net/finder-au/wp-uploads/2018/07/multiplafet.jpg")';
+newParagraph.style.height = '300px';
 
 const margin = document.querySelector('li');
 margin.style.color = 'pink';
 margin.style.fontFamily = 'times new roman';
 margin.style.fontSize = '100px';
+margin.style.backgroundImage = 'url("https://media.giphy.com/media/SggILpMXO7Xt6/giphy.gif")';
+margin.style.height = '300px';
 
 const newHeader3 = document.querySelector('ul');
 newHeader3.style.color = 'orange';
-newHeader3.style.fontSize = '500px';
+newHeader3.style.fontSize = '11px';
 
 const things = document.querySelector('ol');
 things.style.color = 'limegreen';
@@ -31,4 +38,3 @@ things.style.fontSize = '70px';
 
 const newButton = document.querySelector('button');
 newButton.style.color = 'red';
-
